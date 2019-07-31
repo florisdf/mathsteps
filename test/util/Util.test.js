6a665ab071855c935e54f3a6fe17fab93b875c81
florisdf <floris.defeyter@kuleuven.be>
2019-07-31 19:27:44 +0200

    Add haveSameNodes

--- test/util/Util.test.js
+++ test/util/Util.test.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const mathjs = require('mathjs');
 
 const Util = require('../../lib/util/Util');
 
@@ -19,4 +20,25 @@ describe('appendToArrayInObject', function () {
       {'key': ['old_value', 'new_value']}
      );
   });
+});
+
+function testHaveSameNodes(arr1, arr2, expectOut) {
+  it('[' + arr1.join(', ') + '] and [' + arr2.join(', ') + ']'
+    + ' -> ' + expectOut, function () {
+      const arr1Nodes = arr1.map(n => mathjs.parse(n));
+      const arr2Nodes = arr2.map(n => mathjs.parse(n));
+      const out = Util.haveSameNodes(arr1, arr2);
+      assert.equal(out, expectOut);
+  });
+}
+
+describe('have same nodes', function() {
+  const tests = [
+    [ ['x'], ['x'], true ],
+    [ ['x', 'y'], ['y', 'x'], true ],
+    [ ['x', 'y'], ['x'], false ],
+    [ ['x', 'y'], ['x', 'z'], false ],
+    [ ['-2', 'x^2 - 3', '6 - y'], ['6 - y', '-2', 'x^2 - 3'], true ],
+  ];
+  tests.forEach(t => testHaveSameNodes(t[0], t[1], t[2]));
 });
