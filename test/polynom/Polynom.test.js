19238d4468a8ee294cc0e0e94c74e1829e165e29
florisdf <floris.defeyter@kuleuven.be>
2019-08-13 14:42:22 +0200

    Remove isolate and divide_np tests

--- test/polynom/Polynom.test.js
+++ test/polynom/Polynom.test.js
@@ -3,29 +3,6 @@ const assert = require('assert');
 const mathjs = require('mathjs');
 const Polynom = require('../../lib/polynom/Polynom');
 
-
-function testDivide(term, divisor, expectOut) {
-  it(`${term} by ${divisor}` + ' -> ' + expectOut, function () {
-    const inTerm = mathjs.parse(term);
-    const inDivisor = mathjs.parse(divisor);
-    const out = Polynom.divide_np(inTerm, inDivisor);
-    assert.deepEqual(out.toString(), expectOut);
-  });
-}
-
-describe('divide non-poly factor', function() {
-  const tests = [
-    ['6', '3', '2'],
-    ['x^2', 'x', 'x'],
-    ['x^2 + x', 'x', 'x + 1'],
-    ['x^2 - x', 'x', 'x - 1'],
-    ['(x + 1) * x', 'x + 1', 'x'],
-    ['(x + 1) * x + 3* (x + 1)', 'x + 1', 'x + 3'],
-    ['3y^3 - 6y^2', '3y^2', 'y - 2'],
-  ];
-  tests.forEach(t => testDivide(t[0], t[1], t[2]));
-});
-
 function testNegate(node, expectOut) {
   it(`${node} -> ${expectOut}`, function() {
     const inNode = mathjs.parse(node);
@@ -67,26 +44,6 @@ describe('termfacs to node', function() {
     [[['-x'], ['1'], ['-3', 'y']], '-x + 1 - 3 * y', false],
   ];
   tests.forEach(t => testTermFacsToNode(t[0], t[1], t[2]));
-});
-
-
-function testIsolate(node, factor, expectOut) {
-  it(`${factor} from ${node}` + ' -> ' + expectOut, function () {
-    const inNode = mathjs.parse(node);
-    const inFactor = mathjs.parse(factor);
-    const out = Polynom.isolate(inNode, inFactor);
-    assert.deepEqual(out.toString(), expectOut);
-  });
-}
-
-describe('isolate factor', function() {
-  const tests = [
-    ['x^2 + x', 'x', 'x * (x + 1)'],
-    ['2 * (x + 1) - 3x * (1 + x)', 'x + 1', '(x + 1) * (2 - 3 * x)'],
-    ['3y^3 - 6y^2', '3y^2', '3 * y ^ 2 * (y - 2)'],
-    ['3x^2 * y^3 - 6y^2 * x', '3y^2 * x', '3 * y ^ 2 * x * (x * y - 2)'],
-  ];
-  tests.forEach(t => testIsolate(t[0], t[1], t[2]));
 });
 
 
